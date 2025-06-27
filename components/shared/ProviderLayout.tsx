3d96547c6566ed62d57500780672dda43d7e03d4
daksh <akshd@outlook.com>
2025-06-27 12:38:12 +0530

    feat: add siwe

--- components/shared/ProviderLayout.tsx
+++ components/shared/ProviderLayout.tsx
@@ -3,26 +3,34 @@
 import '@rainbow-me/rainbowkit/styles.css';
 
 import {
-    getDefaultConfig,
     RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
 import React from 'react'
 import { WagmiProvider } from 'wagmi'
 import { config } from '@/lib/config'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { GetSiweMessageOptions, RainbowKitSiweNextAuthProvider } from '@rainbow-me/rainbowkit-siwe-next-auth';
+import { SessionProvider } from 'next-auth/react';
 
 const queryClient = new QueryClient()
 
+const getSiweMessageOptions: GetSiweMessageOptions = () => ({
+    statement: 'Sign in to my hyper-alert',
+});
 
 function ProviderLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
             <WagmiProvider config={config}>
-                <QueryClientProvider client={queryClient}>
-                    <RainbowKitProvider coolMode={true} showRecentTransactions={true}>
-                        {children}
-                    </RainbowKitProvider>
-                </QueryClientProvider>
+                <SessionProvider refetchInterval={0}>
+                    <QueryClientProvider client={queryClient}>
+                        <RainbowKitSiweNextAuthProvider getSiweMessageOptions={getSiweMessageOptions}>
+                            <RainbowKitProvider coolMode={true} showRecentTransactions={true}>
+                                {children}
+                            </RainbowKitProvider>
+                        </RainbowKitSiweNextAuthProvider>
+                    </QueryClientProvider>
+                </SessionProvider>
             </WagmiProvider>
         </div>
     )
