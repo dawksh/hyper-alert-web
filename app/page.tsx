be0e9df27f33997a70773ad5d9cdad885f149c6b
Daksh Kulshrestha <akshd@outlook.com>
2025-07-02 01:54:18 +0530

    feat: add signing

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,16 @@
 import Positions from "@/components/home/Positions";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { isConnected } = useAccount()
+  const { data: session } = useSession()
   return (
     <div className="flex flex-col gap-4 p-4 items-center justify-start h-screen">
-      {!isConnected && <p>Connect your wallet to get started</p>}
-      {!isConnected && <ConnectButton />}
-      {isConnected && <Positions />}
+      {!isConnected && !session?.address && <p>Connect your wallet to get started</p>} 
+      {!isConnected && !session?.address && <ConnectButton />}
+      {isConnected && session?.address && <Positions />}
     </div>
   );
 }
--- components/shared/ProviderLayout.tsx
+++ components/shared/ProviderLayout.tsx
@@ -8,8 +8,11 @@ import {
 } from '@rainbow-me/rainbowkit';
 import React from 'react'
 import { WagmiProvider } from 'wagmi'
+import { RainbowKitSiweNextAuthProvider } from '@rainbow-me/rainbowkit-siwe-next-auth';
+
 import { config } from '@/lib/config'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SessionProvider } from 'next-auth/react'
 
 export const queryClient = new QueryClient()
 
@@ -18,9 +21,19 @@ function ProviderLayout({ children }: { children: React.ReactNode }) {
         <div>
             <WagmiProvider config={config}>
                 <QueryClientProvider client={queryClient}>
+                    <SessionProvider>
+                    <RainbowKitSiweNextAuthProvider getSiweMessageOptions={() => {
+                        return {
+                            domain: window.location.host,
+                            statement: "Sign in with Ethereum to the app.",
+                            uri: window.location.origin,
+                        }
+                    }}>
                     <RainbowKitProvider coolMode={true} showRecentTransactions={true}>
                         {children}
                     </RainbowKitProvider>
+                    </RainbowKitSiweNextAuthProvider>
+                    </SessionProvider>
                 </QueryClientProvider>
             </WagmiProvider>
         </div>
