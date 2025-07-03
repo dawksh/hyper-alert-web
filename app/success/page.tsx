0e46b6c786a97a235b0e63631fff96bd1bc8ad79
Daksh Kulshrestha <akshd@outlook.com>
2025-07-04 00:47:17 +0530

    feat: add basic stripe integration

---
+++ app/success/page.tsx
@@ -0,0 +1,36 @@
+import { redirect } from 'next/navigation'
+
+import { stripe } from '../../lib/stripe'
+
+export default async function Success({ searchParams }: { searchParams: { session_id: string } }) {
+  const { session_id } = await searchParams
+
+  if (!session_id)
+    throw new Error('Please provide a valid session_id (`cs_test_...`)')
+
+  const {
+    status,
+    invoice,
+    customer_details,
+    metadata
+  } = await stripe.checkout.sessions.retrieve(session_id, {
+    expand: ['line_items', 'payment_intent']
+  })
+
+  if (status === 'open') {
+    return redirect('/')
+  }
+
+  if (status === 'complete') {
+    return (
+      <section id="success">
+        {metadata?.user}
+        <p>
+          We appreciate your business! A confirmation email will be sent to{' '}
+          {customer_details?.email}. If you have any questions, please email{' '}
+        </p>
+        <a href="mailto:orders@example.com">orders@example.com</a>.
+      </section>
+    )
+  }
+}
