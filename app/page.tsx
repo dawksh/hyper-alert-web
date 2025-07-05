2d599bb49af42ffffb567c23c274ceca88bf3a00
Daksh Kulshrestha <akshd@outlook.com>
2025-07-05 19:29:58 +0530

    feat: finish home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,20 @@ const page = () => (
   <div className="min-h-screen w-full bg-zinc-900 flex flex-col items-center overflow-x-hidden py-4 px-2">
     <div className="w-full max-w-7xl flex flex-col gap-y-2 md:gap-y-4 items-center">
       {/* Hero Section */}
-      <section className="w-full bg-indigo-500 rounded-3xl flex flex-col justify-center items-center min-h-[300px] md:min-h-[unset] py-6 md:py-10 shadow-xl px-8 text-center">
+      <section className="w-full bg-indigo-500 rounded-xl flex flex-col justify-center items-center min-h-[300px] md:min-h-[unset] py-4 md:py-8 shadow-xl px-2 text-center">
         <h1 className="text-white text-5xl md:text-6xl lg:text-8xl font-black leading-tight max-w-4xl mx-auto">
           We send you a call on your phone, before your hyperliquid trades get <span className="text-lime-400">liquidated.</span>
         </h1>
       </section>
       {/* Pricing Section */}
-      <section className="w-full bg-lime-400 rounded-3xl flex flex-col items-center py-5 md:py-8 shadow-xl px-1">
-        <h2 className="text-neutral-900 text-3xl md:text-4xl lg:text-6xl font-bold text-center">
+      <section className="w-full bg-lime-400 rounded-xl flex flex-col items-center py-5 md:py-8 shadow-xl px-1">
+        <h2 className="text-neutral-900 text-3xl md:text-8xl lg:text-8xl font-bold text-center">
           We charge <span className="text-indigo-500">1$/call</span>
         </h2>
       </section>
       {/* How It Works Section */}
-      <section className="w-full grid grid-cols-3 gap-1 md:gap-4">
-        <div className="bg-white rounded-3xl flex flex-col items-start p-1 md:p-4 min-h-[60px] md:min-h-[150px] w-full max-w-[90px] md:max-w-none mx-auto shadow-xl">
+      <section className="w-full grid grid-cols-3 gap-1 md:gap-2">
+        <div className="bg-white rounded-xl flex flex-col items-start p-1 md:p-4 min-h-[20vh] md:min-h-[25vh] w-full max-w-[220px] md:max-w-[33vw] mx-auto shadow-xl">
           <span className="text-neutral-900 text-base md:text-4xl font-normal">
             1.
             <br />
@@ -28,7 +28,7 @@ const page = () => (
             hyperliquid wallet
           </span>
         </div>
-        <div className="bg-white rounded-3xl flex flex-col items-start p-1 md:p-4 min-h-[60px] md:min-h-[150px] w-full max-w-[90px] md:max-w-none mx-auto shadow-xl">
+        <div className="bg-white rounded-xl flex flex-col items-start p-1 md:p-4 min-h-[20vh] md:min-h-[25vh] w-full max-w-[220px] md:max-w-[33vw] mx-auto shadow-xl">
           <span className="text-neutral-900 text-base md:text-4xl font-normal">
             2.
             <br />
@@ -42,7 +42,7 @@ const page = () => (
             on.
           </span>
         </div>
-        <div className="bg-red-600 rounded-3xl flex flex-col items-start p-1 md:p-4 min-h-[60px] md:min-h-[150px] w-full max-w-[90px] md:max-w-none mx-auto shadow-xl">
+        <div className="bg-red-500 rounded-xl flex flex-col items-start p-1 md:p-4 min-h-[20vh] md:min-h-[25vh] w-full max-w-[220px] md:max-w-[33vw] mx-auto shadow-xl">
           <span className="text-neutral-900 text-base md:text-4xl font-normal">
             3.
             <br />
@@ -54,11 +54,11 @@ const page = () => (
         </div>
       </section>
       {/* Talk to us on Section */}
-      <section className="w-full bg-white rounded-3xl flex flex-col md:flex-row items-start md:items-center py-5 md:py-8 px-4 md:px-10 shadow-xl gap-2 md:gap-4 justify-center mb-4">
-        <span className="text-neutral-900 text-3xl md:text-4xl lg:text-5xl font-bold">
+      <section className="w-full bg-indigo-500 rounded-xl flex flex-col md:flex-col items-start md:items-start md:justify-end py-5 md:py-8 px-4 md:px-10 shadow-xl gap-2 md:gap-4 h-[60vh] justify-center mb-4">
+        <span className="text-black text-6xl md:text-8xl lg:text-8xl font-bold">
           Talk to us on:
         </span>
-        <span className="text-indigo-500 text-3xl md:text-4xl lg:text-5xl font-bold mt-1 md:mt-0">
+        <span className="text-white text-6xl md:text-8xl lg:text-8xl font-bold mt-1 md:mt-0">
           <Link href="https://x.com/perpalert" target="_blank">
             Twitter
           </Link>
