88ee3c810c86f9d8969ae4170100859a895d16f3
Daksh Kulshrestha <akshd@outlook.com>
2025-07-05 21:04:04 +0530

    chore: small fix

--- components/home/Positions.tsx
+++ components/home/Positions.tsx
@@ -106,7 +106,7 @@ const Positions = () => {
                   ? "Active"
                   : activeFilter === "inactive"
                   ? "Inactive"
-                  : "Status"}
+                  : "Alert Status"}
                 <svg className="ml-2 w-4 h-4" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" /></svg>
               </Button>
               {statusOpen && (
